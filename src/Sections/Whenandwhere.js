2d3938707ea4ec1ae1a305dad985994b43082a20
Adrita Sharma <adritasharma.developer@gmail.com>
2021-04-12 21:03:07 +0530

    address chenged

--- src/Sections/Whenandwhere.js
+++ src/Sections/Whenandwhere.js
@@ -2,23 +2,11 @@ import React, { Component, Fragment } from "react";
 import { withGoogleMap, GoogleMap, Marker } from "react-google-maps";
 export class Whenandwhere extends Component {
   render() {
-    // const GoogleMapExample = withGoogleMap((props) => (
-    //   <GoogleMap
-    //     defaultCenter={{ lat: props.lat, lng: props.lng }}
-    //     defaultZoom={13}
-    //     text="My Marker"
-    //   >
-    //     {props.isMarkerShown && (
-    //       <Marker position={{ lat: props.lat, lng: props.lng }} />
-    //     )}
-    //   </GoogleMap>
-    // ));
-
     var locations = [
       {
         eventName: "Wedding",
         location:
-          "45/46 RG Baruah Road, Zoo Rd, Tiniali, Guwahati, Assam 781024",
+          "Andhu Rabha Path, Odalbakra, Guwahati, Assam, India, Pin Code:-781034",
         lat: 26.144518,
         lng: 91.736237,
         map:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3581.79535001261!2d91.74975601502868!3d26.138214883467267!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x375a5be326f6ad57%3A0x92660338e7b7c50b!2sAndhu%20Rabha%20Path%2C%20Odalbakra%2C%20Kahilipara%2C%20Guwahati%2C%20Assam%20781034!5e0!3m2!1sen!2sin!4v1612437687186!5m2!1sen!2sin" 
@@ -26,7 +14,7 @@ export class Whenandwhere extends Component {
       {
         eventName: "Reception",
         location:
-          "Rail Colony DIMAPUR, NAGALAND (NL), India (IN), Pin Code:- 797112",
+          "Rail Colony, Dimapur, Nagaland, India, Pin Code:- 797112",
         lat: 25.862989,
         lng: 93.75367,
         map:"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3588.9427570513544!2d93.72608381502386!3d25.904252233572276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3745e1f402a8e455%3A0x69b908ca1ee4f4c1!2sRam%20Thakur%20Mandir%2C%20Railway%20Colony%2C%20Dimapur%2C%20Nagaland%20797112!5e0!3m2!1sen!2sin!4v1612436144629!5m2!1sen!2sin"
